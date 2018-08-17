dc5bfe79dce52954870845a4a7eecb323ff36771
gabrielfreire <gabriel.freire@imsmaxims.com>
2018-08-17 15:52:16 +0100

    fix model

--- model.py
+++ model.py
@@ -8,11 +8,10 @@ class Net(nn.Module):
         super(Net, self).__init__()
 
         self.relu = nn.ReLU()
-        self.conv1 = nn.Conv2d(1, 128, (5, 5), (1, 1), (2, 2))
-        self.conv2 = nn.Conv2d(128, 256, (3, 3), (1, 1), (1, 1))
-        self.conv3 = nn.Conv2d(256, 128, (3, 3), (1, 1), (1, 1))
-        self.conv4 = nn.Conv2d(128, 64, (3, 3), (1, 1), (1, 1))
-        self.conv5 = nn.Conv2d(64, upscale_factor ** 2, (3, 3), (1, 1), (1, 1))
+        self.conv1 = nn.Conv2d(1, 64, (8, 8), (1, 1), (2, 2))
+        self.conv2 = nn.Conv2d(64, 64, (5, 5), (1, 1), (1, 1))
+        self.conv3 = nn.Conv2d(64, 32, (5, 5), (1, 1), (1, 1))
+        self.conv4 = nn.Conv2d(32, upscale_factor ** 2, (5, 5), (1, 1), (1, 1))
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
 
         self._initialize_weights()
@@ -21,13 +20,11 @@ class Net(nn.Module):
         x = self.relu(self.conv1(x))
         x = self.relu(self.conv2(x))
         x = self.relu(self.conv3(x))
-        x = self.relu(self.conv4(x))
-        x = self.pixel_shuffle(self.conv5(x))
+        x = self.pixel_shuffle(self.conv4(x))
         return x
 
     def _initialize_weights(self):
         init.orthogonal_(self.conv1.weight, init.calculate_gain('relu'))
         init.orthogonal_(self.conv2.weight, init.calculate_gain('relu'))
         init.orthogonal_(self.conv3.weight, init.calculate_gain('relu'))
-        init.orthogonal_(self.conv4.weight, init.calculate_gain('relu'))
-        init.orthogonal_(self.conv5.weight)
+        init.orthogonal_(self.conv4.weight)
